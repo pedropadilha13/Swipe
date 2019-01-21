fe88097e94c888555a228e0942f42572745fd44c
Pedro Padilha <pedropadilha@me.com>
2019-01-21 14:05:17 -0200

    First commit

---
+++ src/Deck.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { View, Animated, Text } from "react-native";
+
+class Deck extends Component {
+  render() {
+    return (
+      <View>
+        <Text>lalala</Text>
+      </View>
+    );
+  }
+}
+
+const styles = {};
+
+export default Deck;
